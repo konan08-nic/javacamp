f1e696c63012a84deb96140c34802bb7919dafd5
Toure A. Karim <you@programmer.net>
2020-06-05 11:50:21 +0300

    add new alternative solution

---
+++ alt/SO-0003.java
@@ -0,0 +1,20 @@
+package javacamp;
+
+public class SO0001 {
+
+	public static void main(String[] args) {
+		
+		int ppcm = determinePPCM(20);
+                System.out.println("Le plus petit multiple de chacun des nombres de 1 à %i: %i ",20,ppcm);
+			            
+             }
+
+      public static int determinePPCM(int n){
+
+           // Ecrire le code de la fonction ici
+
+
+      }	
+
+}
+		
