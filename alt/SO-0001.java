3e5dcb378b657c1d3ca72af2879715834da951f7
trabdlkarim <trabdlkarim@programmer.net>
2020-06-05 18:21:23 +0300

    add alternative solution #0001

--- alt/SO-0001.java
+++ alt/SO-0001.java
@@ -1,18 +1,32 @@
 package javacamp;
+import java.util.List;
 
 public class SO0001 {
 
 	public static void main(String[] args) {
-		
-		int somme = multiple(1000);
-                System.out.println("La somme des multiples de 3 et 5 inferieurs a 1000: " + somme);
+	   List<Integer> list = multiplesDeTroisEtCinq(1000);
+           System.out.println(list);
+           System.out.println(somme(list));
+           System.out.println("La somme des multiples de 3 et 5 inferieurs a 1000: " + somme);
 			            
              }
 
-      public static int multiple(int n){
-
-           // Ecrire le code de la fonction ici
-
+    public static List  multiplesDeTroisEtCinq(int n){
+        
+     List multiples = new ArrayList() ;
+     for (int i=6;i<n;i++) 
+         if( (i%3==0) || (i%5==0))
+             multiples.add(i);
+     return multiples;
+    }
+    
+    
+    public static int somme(List<Integer> list){
+        int somme = 0;
+        somme = list.stream().map((i) -> i).reduce(somme, Integer::sum);
+        return somme;
+     
+    }
 
       }	
 
