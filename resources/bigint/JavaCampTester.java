778ad19d270156bc547cb12f71d86ea619103562
Habdool Khareem <trabdlkarim@programmer.net>
2020-06-06 08:57:16 +0300

    Update JavaCampTester.java

--- resources/bigint/JavaCampTester.java
+++ resources/bigint/JavaCampTester.java
@@ -9,8 +9,6 @@ import java.math.BigInteger;
  
 public class JavaCampTester {
     public static void main(String[] args) {
-       
-        S0004 sol = new S0004();
         
         BigInteger bigint1 = new BigInteger("2024");
         BigInteger bigint2 = new BigInteger("1994");
@@ -21,17 +19,28 @@ public class JavaCampTester {
         result = bigint1.add(bigint2);
         System.out.println(String.format("%d + %d = %d",bigint1,bigint2,result));
         
+        // Soustraction
         result = bigint1.subtract(bigint2);
         System.out.println(String.format("%d - %d = %d",bigint1,bigint2,result));
+        
+        // Multiplication
         result = bigint1.multiply(bigint2);
         System.out.println(String.format("%d * %d = %d",bigint1,bigint2,result));
         
+        // Division Entière
         result = bigint1.divide(bigint2);
         System.out.println(String.format("%d / %d = %d",bigint1,bigint2,result));
         
+        // Modulo
         result = bigint1.mod(bigint2);
         System.out.println(String.format("%d mod %d = %d",bigint1,bigint2,result));
         
+        /* Division sans reste - cet opérateur renvoie un tableau contenant deux 
+         objects de type BingInteger, le premier est le quotient entier de la 
+         division et le second est le reste de la division.
+         C'est en effet un opérateur pratique pour avoir le quotient et le 
+         reste de la division en seul coup.
+         */
         tab = bigint1.divideAndRemainder(bigint2);
         System.out.println(String.format("%d // %d = %d",bigint1,bigint2,tab[0]));
         System.out.println(String.format("%d mod %d = %d",bigint1,bigint2,tab[1]));
