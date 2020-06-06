cb8a0d320e5311d2d1e201f14dabf6988f0aa95a
Habdool Khareem <trabdlkarim@programmer.net>
2020-06-06 08:25:10 +0300

    Update S0004.java

--- tips/S0004.java
+++ tips/S0004.java
@@ -1,22 +1,23 @@
 package javacamp;
+import java.math.BigInteger;
 
 public class S0004 {
 
 	public static void main(String[] args) {
-	       int p = puissance(2,100)	
+	       BigInteger p = puissance(2,100)	
                int s = sommeDesChiffres(p)
                System.out.println(String.format("puissance(%d,%d) = %d",2,1000,p));
                System.out.println(String.format("somme(%d) = %d",p,s));     
         }
 
-        public static int puissance(int a,int n){
+        public static BigInteger puissance(int a,int n){
 
            // Ecrire ton code  ici
 
 
       }	
 
-      public static int sommeDesChiffres(int n){
+      public static int sommeDesChiffres(BigInteger n){
 
         // Ecrire ton ici
 
