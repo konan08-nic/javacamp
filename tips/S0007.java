67b3bbc0915ee6743a03432772e1149000abb3e6
Habdool Khareem <trabdlkarim@programmer.net>
2020-06-11 17:37:22 +0300

    Update and rename SO-0007.java to S0007.java

---
+++ tips/S0007.java
@@ -0,0 +1,31 @@
+package javacamp;
+import java.util.ArrayList;
+public class S0007 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> premiers = trouveNombresPremiers(1000);
+		ArrayList<Integer> facteurs = trouveFacteursPremiers(1000);
+		
+                System.out.print("La liste des nombres premiers inferieurs à 1000: ");
+		System.out.println(premiers);
+		
+		System.out.print("La liste des facteurs premiers de 1000: ");
+		System.out.println(premiers);
+			            
+             }
+
+      public static ArrayList<Integer> trouveNombresPremiers(int n){
+
+           // Ecrire le code de la methode ici
+
+
+      }	
+	
+    public static ArrayList<Integer> trouveFacteursPremiers(int n){
+    
+            // Ecrire le code de la methode ici
+    }
+
+}
+		
