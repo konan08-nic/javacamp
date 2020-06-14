6cdc7bf2b039a4ff12794d2ff31403ac1e73e9d6
Habdool Khareem <trabdlkarim@programmer.net>
2020-06-14 23:04:38 +0300

    Update Commands.java

--- tips/S0008/Commands.java
+++ tips/S0008/Commands.java
@@ -1,5 +1,31 @@
+package javacamp;
 class Commands{
 
+  public int addition(int a, int b){
+     // Ecrire ton ici
+  }
+  
+  public int soustraction(int a, int b){
+    // Ecrire ton ici
+  }
 
+public int multiplication(int a, int b){
+     // Ecrire ton ici
+  }
+
+public int division(int a, int b){
+    // Ecrire ton ici
+  }
+
+  public int modulo(int a, int b){
+     // Ecrire ton ici
+  }
+
+public void aide(int a, int b){
+   // Ecrire ton ici
+  }
+public void quit(int a, int b){
+   // Ecrire ton ici
+  }
 
 }
