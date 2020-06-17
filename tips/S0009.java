ff293151eedd031429826e7a6e68e291ff5e8b2c
Habdool Khareem <trabdlkarim@programmer.net>
2020-06-17 23:53:40 +0300

    Update S0009.java

--- tips/S0009.java
+++ tips/S0009.java
@@ -1,15 +1,20 @@
 package javacamp;
 
-public class S0001 {
+public class S0009 {
 
 	public static void main(String[] args) {
 		
-		int somme = multiple(1000);
-                System.out.println("La somme des multiples de 3 et 5 inferieurs a 1000: " + somme);
+		int somme = sommeDesChiffres(puissance(2,100));
+                System.out.println("La somme des chiffres de 2 puissance 100 est: " + somme);
 			            
              }
 
-      public static int multiple(int n){
+	
+      public static BigInteger puissance(int a,int n){
+       // Ecrire le code de la fonction ici
+      
+      }
+      public static int sommeDesChiffres(BigInteger n){
 
            // Ecrire le code de la fonction ici
 
